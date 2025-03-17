de3146fbaa36bedb7759ef2e26f67f8fc1ccae3a
komiyamma <komiyama0629@gmail.com>
2025-03-18 04:02:15 +0900

    高速ないい感じの Unicode 正規化

---
+++ HmNormToStandardChar/HmNormToStandardChar.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace HmNormToStandardChar;
+
+[ComVisible(true)]
+[ClassInterface(ClassInterfaceType.None)]
+[Guid("45C935C4-A35E-49DE-8373-D058AE8574C8")]
+public class HmNormToStandardChar
+{
+    public string Normalize(string text)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return text;
+            }
+
+            var lines = text.Split('\n');
+
+            Parallel.For(0, lines.Length, i =>
+            {
+                lines[i] = NormalizeLine(lines[i]);
+            });
+
+            return String.Join("\n", lines);
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+    }
+
+    private Encoding encoder = Encoding.GetEncoding("Shift_JIS");
+    private bool CanConvertSJIS(string text)
+    {
+        try
+        {
+            var encodedBytes = encoder.GetBytes(text);
+            var decodedText = encoder.GetString(encodedBytes);
+            return text == decodedText;
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
+
+    private string NormalizeLine(string text)
+    {
+        if (CanConvertSJIS(text))
+        {
+            return text;
+        }
+
+        var walker = System.Globalization.StringInfo.GetTextElementEnumerator(text);
+
+        var dstText = "";
+        while(walker.MoveNext()) {
+            var chr = walker.GetTextElement();
+            dstText += NormalizeCharacter(chr);
+        }
+        return dstText;
+    }
+
+    private string NormalizeCharacter(string text)
+    {
+        if (CanConvertSJIS(text))
+        {
+            return text;
+        }
+
+        var textNFC = text.Normalize(System.Text.NormalizationForm.FormC);
+        if (CanConvertSJIS(textNFC))
+        {
+            return textNFC;
+        }
+        if (text != textNFC)
+        {
+            return textNFC;
+        }
+
+        return text.Normalize(System.Text.NormalizationForm.FormKC);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# HmNormToStandardChar
+
+![HmNormToStandardChar v1.0.0](https://img.shields.io/badge/HmNormToStandardChar-v1.0.0-6479ff.svg)
+[![MIT](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
+
+いい感じで Unicode正規化をするマクロ。
+カスタム性に欠けるがかなり高速。
